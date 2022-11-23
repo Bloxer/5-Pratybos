ff2a0078d40f036038a904ce4099c850efc47e5b
Memzo <danasniedzis@gmail.com>
2022-11-23 03:56:04 +0200

    Add files via upload

---
+++ P5/P5-2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace P5_2
+{
+    class Program
+    {
+        const string CFd = "..\\..\\U1.txt";
+        const string CFr = "..\\..\\Rezultatai.txt";
+        const int CMax = 256;
+        static void Main(string[] args)
+        {
+            string[] eilutes = new string[CMax];
+            eilutes[0] = "";
+            Skaityti(CFd, ref eilutes);
+            Spausdinti(CFr, eilutes);
+        }
+        static void Skaityti(string fv, ref string[] eilutes)
+        {
+            int i = 0;
+            string[] lines = File.ReadAllLines(fv);
+            foreach (string line in lines)
+            {
+                if (line.Length != 0)
+                {
+                    eilutes[i] = line.ToString();
+                    i++;
+                }
+            }
+
+        }
+        static void Spausdinti(string fvr, string[] eilutes)
+        {
+            using (var fr = File.CreateText(fvr))
+            {
+                foreach (string line in eilutes)
+                {
+                    fr.WriteLine(line);
+                }
+            }
+        }
+
+    }
+}
